e694b36edf2a9def6481feb71a3fb2e66a5d8db6
Andreas Leoncedis <info.andreasleoncedis@gmail.com>
2022-09-30 11:10:10 +0100

    navbar animation phase 1

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const navSlide = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
+  burger.addEventListener("click", () => {
+    // TOGGLE NAV
+    nav.classList.toggle("nav-active");
+
+    // ANIMATE LINKS
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 0}`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
